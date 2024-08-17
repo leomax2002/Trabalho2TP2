3d744418ee048bc61d52bb843778edbca4c71f58
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2024-08-17 14:48:45 -0300

    Update FLangParserTest.scala

--- src/test/scala/br/unb/cic/flang/FLangParserTest.scala
+++ src/test/scala/br/unb/cic/flang/FLangParserTest.scala
@@ -7,7 +7,7 @@ import Declarations._
 import FLangParser._
 
 class FLangParserTest extends AnyFlatSpec with should.Matchers {
-  val inc = FDeclaration("inc", "x", Add(Id("x"), CInt(1))) 
+  val inc = FuncDecl("inc", "x", Add(Id("x"), CInt(1)))
 
   "The FLangParser" should "parse integer literals" in {
     val input = "5"
@@ -42,5 +42,21 @@ class FLangParserTest extends AnyFlatSpec with should.Matchers {
     val result = FLangParser.parseExpr(input)
     result shouldBe a[FLangParser.Success[_]]
     result.get shouldEqual IfThenElse(CInt(0), CInt(1), CInt(2))
+  }
+
+  it should "parse function declarations" in {
+    val input = "func inc(x) = x + 1"
+    val result = FLangParser.parseExpr(input)
+    result shouldBe a[FLangParser.Success[_]]
+    result.get shouldEqual FuncDecl("inc", "x", Add(Id("x"), CInt(1)))
+  }
+
+  it should "parse nested function declarations" in {
+    val input = "func double(x) = func add(y) = y + y"
+    val result = FLangParser.parseExpr(input)
+    result shouldBe a[FLangParser.Success[_]]
+    result.get shouldEqual 
+      FuncDecl("double", "x", 
+        FuncDecl("add", "y", Add(Id("y"), Id("y"))))
   }
 }
