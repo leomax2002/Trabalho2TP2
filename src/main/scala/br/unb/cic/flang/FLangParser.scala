5f82b121ab064e7c24ef3d08e54a5cc5fbb6cd2b
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2024-08-17 14:48:11 -0300

    Update FLangParser.scala

--- src/main/scala/br/unb/cic/flang/FLangParser.scala
+++ src/main/scala/br/unb/cic/flang/FLangParser.scala
@@ -2,20 +2,19 @@ package br.unb.cic.flang
 
 import scala.util.parsing.combinator._
 import Declarations._
-import EH_and_StateMonad._
 
 object FLangParser extends RegexParsers {
   def int: Parser[CInt] = """\d+""".r ^^ { s => CInt(s.toInt) }
   def variable: Parser[Id] = """[a-zA-Z][a-zA-Z0-9]*""".r ^^ Id
 
-  def expr: Parser[Expr] = ifExpr | addExpr | mulExpr | appExpr | int | variable
+  def expr: Parser[Expr] = ifExpr | funcDecl | addExpr | mulExpr | appExpr | int | variable 
   def factor: Parser[Expr] = int | variable | "(" ~> expr <~ ")"  
   def term: Parser[Expr] = factor ~ rep("*" ~ factor) ^^ {
     case firstFactor ~ restFactors => restFactors.foldLeft(firstFactor) {
       case (acc, "*" ~ factor) => Mul(acc, factor)
     }
-  }  
-  
+  }
+
   def addExpr: Parser[Expr] = term ~ rep("+" ~ term) ^^ {
     case firstTerm ~ restTerms => restTerms.foldLeft(firstTerm) {
       case (acc, "+" ~ term) => Add(acc, term)
@@ -36,6 +35,10 @@ object FLangParser extends RegexParsers {
     case "if" ~ cond ~ "then" ~ th ~ "else" ~ el => IfThenElse(cond, th, el)
   }
 
+  //func inc(x) = x + 1
+  def funcDecl: Parser[Expr] = "func" ~ variable ~ "(" ~ variable ~ ")" ~ "=" ~ expr ^^ {
+    case "func" ~ name ~ "(" ~ param ~ ")" ~ "=" ~ body => FuncDecl(name.name, param.name, body)
+  }
+
   def parseExpr(input: String): ParseResult[Expr] = parseAll(expr, input)
 }
-
