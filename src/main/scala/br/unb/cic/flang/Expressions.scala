6b349907ff3f7d58784746a05f1d3d6c2bf2a042
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2024-08-17 14:48:22 -0300

    Update Expressions.scala

--- src/main/scala/br/unb/cic/flang/Expressions.scala
+++ src/main/scala/br/unb/cic/flang/Expressions.scala
@@ -8,3 +8,4 @@ case class Mul(lhs: Expr, rhs: Expr) extends Expr
 case class Id(name: String) extends Expr
 case class App(name: String, arg: Expr) extends Expr
 case class IfThenElse(cond: Expr, th: Expr, el: Expr) extends Expr
+case class FuncDecl(name: String, param: String, body: Expr) extends Expr
